a442e370f6b56eaeeb69d020f73a95e6c07f1050
Chris Woolner <chris.woolner@gmail.com>
2009-07-05 01:01:18 -0400

    Add the @Repository annotation so that Spring would autowire it.

--- java/src/main/java/com/flexpoker/dao/GameDaoImpl.java
+++ java/src/main/java/com/flexpoker/dao/GameDaoImpl.java
@@ -1,7 +1,10 @@
 package com.flexpoker.dao;
 
+import org.springframework.stereotype.Repository;
+
 import com.flexpoker.model.Game;
 
+@Repository("gameDao")
 public class GameDaoImpl extends GenericDaoImpl<Game, Integer> implements GameDao {
 
 }
