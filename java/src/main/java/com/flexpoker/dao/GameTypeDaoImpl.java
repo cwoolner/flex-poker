7c361aac11ca86cecff6463c00222fabc24c0295
Chris Woolner <chris.woolner@gmail.com>
2009-07-06 02:43:13 -0400

    Add the missing @Repository annotation.

--- java/src/main/java/com/flexpoker/dao/GameTypeDaoImpl.java
+++ java/src/main/java/com/flexpoker/dao/GameTypeDaoImpl.java
@@ -1,7 +1,10 @@
 package com.flexpoker.dao;
 
+import org.springframework.stereotype.Repository;
+
 import com.flexpoker.model.GameType;
 
+@Repository("gameTypeDao")
 public class GameTypeDaoImpl extends GenericDaoImpl<GameType, Integer> implements GameTypeDao {
 
 }
