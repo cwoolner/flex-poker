ffb7f48af3cd8b409ac064d80d2fb91f7b024ce1
Chris Woolner <chris.woolner@gmail.com>
2009-07-15 23:25:37 -0400

    Add equals() and hashcode() methods.

--- java/src/main/java/com/flexpoker/model/User.java
+++ java/src/main/java/com/flexpoker/model/User.java
@@ -16,6 +16,8 @@ import org.springframework.security.userdetails.UserDetails;
 @Table(name = "users")
 public class User implements UserDetails, Principal {
 
+    private static final long serialVersionUID = -271676872192235370L;
+
     private Integer id;
 
     private String username;
@@ -85,6 +87,31 @@ public class User implements UserDetails, Principal {
     @Transient
     public String getName() {
         return username;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        User other = (User) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 
 }
