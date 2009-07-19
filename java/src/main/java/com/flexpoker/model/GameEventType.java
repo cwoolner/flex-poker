200791e05f821cbf83bcd17ddd64bd3da6defaeb
Chris Woolner <chris.woolner@gmail.com>
2009-07-19 06:18:51 -0400

    Add the different event types as constants.

--- java/src/main/java/com/flexpoker/model/GameEventType.java
+++ java/src/main/java/com/flexpoker/model/GameEventType.java
@@ -9,6 +9,28 @@ import javax.persistence.Table;
 @Table(name = "gameEventTypes")
 public class GameEventType {
 
+    public static final String HAND_DEALT = "hand dealt";
+
+    public static final String CHECK = "check";
+    
+    public static final String FOLD = "fold";
+
+    public static final String BET = "bet";
+
+    public static final String RAISE = "raise";
+
+    public static final String HAND_COMPLETE = "hand complete";
+
+    public static final String WON_HAND = "won hand";
+
+    public static final String PLAYER_OUT = "player out";
+
+    public static final String FLOP_DEALT = "flop dealt";
+
+    public static final String TURN_DEALT = "turn dealt";
+
+    public static final String RIVER_DEALT = "river dealt";
+
     private Integer id;
 
     private String name;
