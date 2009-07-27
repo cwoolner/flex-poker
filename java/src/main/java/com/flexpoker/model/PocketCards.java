b8dbabf02b1f26cc722027c1bcd6916c6a82450f
Chris Woolner <chris.woolner@gmail.com>
2009-07-27 00:06:57 -0400

    Add default constructor and setter methods so that BlazeDS can properly serialize between AS and Java.

--- java/src/main/java/com/flexpoker/model/PocketCards.java
+++ java/src/main/java/com/flexpoker/model/PocketCards.java
@@ -10,13 +10,24 @@ public class PocketCards {
         this.card1 = card1;
         this.card2 = card2;
     }
-    
+
+    public PocketCards() {
+    }
+
     public Card getCard1() {
         return card1;
     }
 
+    public void setCard1(Card card1) {
+        this.card1 = card1;
+    }
+
     public Card getCard2() {
         return card2;
+    }
+
+    public void setCard2(Card card2) {
+        this.card2 = card2;
     }
 
 }
