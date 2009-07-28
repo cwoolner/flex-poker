e315d2c4a92cef9a65091d31d51d5f68211dcf6a
Chris Woolner <chris.woolner@gmail.com>
2009-07-28 00:37:01 -0400

    Organize imports.

--- java/src/main/java/com/flexpoker/bso/GameEventBsoImpl.java
+++ java/src/main/java/com/flexpoker/bso/GameEventBsoImpl.java
@@ -2,9 +2,7 @@ package com.flexpoker.bso;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Random;
 import java.util.Set;
 
