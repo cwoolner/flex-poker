5b9ff502bf05bdef37fdb3764e2a433a6c7a858f
Chris Woolner <chris.woolner@gmail.com>
2009-09-29 05:31:54 -0400

    Add new, totally unimplemented test class for FlexControllerImpl.

---
+++ java/src/test/java/com/flexpoker/controller/FlexControllerImplTest.java
@@ -0,0 +1,110 @@
+package com.flexpoker.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class FlexControllerImplTest {
+
+    @Test
+    public void testCreateGame() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testFetchAllGames() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testJoinGame() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testFetchAllUserGameStatuses() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testVerifyRegistrationForGame() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testVerifyGameInProgress() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testFetchTable() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testFetchPocketCards() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testCheck() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testFetchFlopCards() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testFetchRiverCard() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testFetchTurnCard() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testFetchOptionalShowCards() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testFetchRequiredShowCards() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetGameBso() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetGameBso() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetGameEventBso() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetGameEventBso() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testGetEventManager() {
+        fail("Not yet implemented");
+    }
+
+    @Test
+    public void testSetEventManager() {
+        fail("Not yet implemented");
+    }
+
+}
