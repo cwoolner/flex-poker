15e101db0f62fe6390ad2ee8917b8ed0cf9d220a
Chris Woolner <chris.woolner@gmail.com>
2009-11-01 08:19:17 -0500

    Move ACE to the end so that it has the highest ordinal value.

--- java/src/main/java/com/flexpoker/model/CardRank.java
+++ java/src/main/java/com/flexpoker/model/CardRank.java
@@ -2,6 +2,6 @@ package com.flexpoker.model;
 
 public enum CardRank {
 
-    ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING;
+    TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE;
 
 }
