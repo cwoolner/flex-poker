84a5f16c7919c0ec7d6de15e804a6533fece4660
Chris Woolner <chris.woolner@gmail.com>
2010-01-08 18:59:58 -0500

    Move some of the logic in decrementTime() to a new handleSeat() method just to make it clearer.  Also add some logic so that the player will check instead of fold if their callAmount is 0.

--- java/src/main/java/com/flexpoker/controller/ActionOnTimerControllerImpl.java
+++ java/src/main/java/com/flexpoker/controller/ActionOnTimerControllerImpl.java
@@ -23,28 +23,45 @@ public class ActionOnTimerControllerImpl implements ActionOnTimerController {
 
     @Override
     public void decrementTime() {
-        Map<Game, Map<Table, Map<Seat, Integer>>> actionOnTimerMap =
-                actionOnTimerBso.getActionOnTimerMap();
+        synchronized (this) {
+            Map<Game, Map<Table, Map<Seat, Integer>>> actionOnTimerMap =
+                    actionOnTimerBso.getActionOnTimerMap();
 
-        for (Game game : actionOnTimerMap.keySet()) {
-            for (Table table : actionOnTimerMap.get(game).keySet()) {
-                Map<Seat, Integer> seatMap = actionOnTimerMap.get(game).get(table);
-                if (seatMap.size() != 1) {
-                    throw new IllegalArgumentException("Timer should only be on "
-                            + "one user per table.");
-                }
-                for (Seat seat : seatMap.keySet()) {
-                    Integer currentCount = seatMap.get(seat);
-                    if (currentCount == 0) {
-                        HandState handState = playerActionsBso.fold(game, table,
-                                seat.getUserGameStatus().getUser());
-                        eventManager.sendFoldEvent(game, table, handState,
-                                seat.getUserGameStatus().getUser().getUsername());
-                    } else {
-                        currentCount = currentCount - 1;
-                        seatMap.put(seat, currentCount);
+            for (Game game : actionOnTimerMap.keySet()) {
+                for (Table table : actionOnTimerMap.get(game).keySet()) {
+                    Map<Seat, Integer> seatMap = actionOnTimerMap.get(game).get(table);
+                    if (seatMap.size() > 1) {
+                        throw new IllegalArgumentException("Timer should only be on "
+                                + "one user per table.");
                     }
+                    handleSeat(game, table, seatMap);
                 }
+            }
+        }
+    }
+
+    private void handleSeat(Game game, Table table, Map<Seat, Integer> seatMap) {
+        for (Seat seat : seatMap.keySet()) {
+            Integer currentCount = seatMap.get(seat);
+            if (currentCount == 0) {
+                actionOnTimerBso.removeSeat(game, table, seat);
+
+                if (seat.getCallAmount() == 0) {
+                    // if they only need to check, then just check
+                    HandState handState = playerActionsBso.check(game, table,
+                            seat.getUserGameStatus().getUser());
+                    eventManager.sendCheckEvent(game, table, handState,
+                            seat.getUserGameStatus().getUser().getUsername());
+                } else {
+                    // otherwise fold them
+                    HandState handState = playerActionsBso.fold(game, table,
+                            seat.getUserGameStatus().getUser());
+                    eventManager.sendFoldEvent(game, table, handState,
+                            seat.getUserGameStatus().getUser().getUsername());
+                }
+            } else {
+                currentCount = currentCount - 1;
+                seatMap.put(seat, currentCount);
             }
         }
     }
