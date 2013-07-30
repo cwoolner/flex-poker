79c7cd8dc4e86de98e7c2d511e17cdddc3734292
Chris Woolner <chris.woolner@gmail.com>
2013-07-30 02:06:41 -0400

    Remove the interfaces for the various old flex controllers and rename the Impl's to the name without the suffix.  Move WebConfig to a new config package and the WebInitializer class to a new init package.

--- src/main/java/com/flexpoker/controller/EventManager.java
+++ src/main/java/com/flexpoker/controller/EventManager.java
@@ -2,34 +2,242 @@ package com.flexpoker.controller;
 
 import java.util.List;
 
+import javax.jms.JMSException;
+import javax.jms.MapMessage;
+
+import org.apache.activemq.command.ActiveMQMapMessage;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.stereotype.Controller;
+
 import com.flexpoker.model.Game;
+import com.flexpoker.model.HandRoundState;
 import com.flexpoker.model.HandState;
 import com.flexpoker.model.Table;
 
-public interface EventManager {
+@Controller("eventManager")
+public class EventManagerImpl implements EventManager {
 
-    void sendGamesUpdatedEvent();
+    private static final String GAMES_UPDATED = "gamesUpdated";
 
-    void sendUserJoinedEvent(Game game, String username, boolean gameAtUserMax);
+    private static final String GAME_STATUS_UPDATES = "gameStatusUpdates";
 
-    void sendChatEvent(String username, String text) ;
+    private static final String TABLE_STATUS_UPDATES = "tableStatusUpdates";
 
-    void sendGameStartingEvent(Game game);
+    private static final String JMS_CHAT = "jms-chat";
 
-    void sendGameInProgressEvent(Game game);
+    private static final String USER_JOINED_GAME = "userJoinedGame";
 
-    void sendGameIsFinishedEvent(Game game);
+    private static final String GAME_IS_STARTING = "gameIsStarting";
 
-    void sendNewHandStartingEvent(Game game, Table table);
+    private static final String GAME_IN_PROGRESS = "gameInProgress";
 
-    void sendNewHandStartingEventForAllTables(Game game, List<Table> tables);
+    private static final String GAME_IS_FINISHED = "gameIsFinished";
 
-    void sendCheckEvent(Game game, Table table, HandState handState, String username);
+    private static final String NEW_HAND_STARTING = "newHandStarting";
 
-    void sendFoldEvent(Game game, Table table, HandState handState, String username);
+    private static final String USER_ACTED = "userActed";
 
-    void sendCallEvent(Game game, Table table, HandState handState, String username);
+    private static final String DEAL_FLOP = "dealFlop";
 
-    void sendRaiseEvent(Game game, Table table, HandState handState, String username);
+    private static final String DEAL_TURN = "dealTurn";
+
+    private static final String DEAL_RIVER = "dealRiver";
+
+    private static final String HAND_COMPLETE = "handComplete";
+
+    @Override
+    public void sendGamesUpdatedEvent() {
+//        messageTemplate.send(GAMES_UPDATED, null);
+    }
+
+    @Override
+    public void sendUserJoinedEvent(Game game, String username,
+            boolean gameAtUserMax) {
+//        messageTemplate.send(new GameStatusMessageCreator(game, USER_JOINED_GAME));
+        sendChatEvent("System", username + " joined Game " + game.getId() + ".");
+
+        if (gameAtUserMax) {
+            sendGamesUpdatedEvent();
+            sendGameStartingEvent(game);
+        }
+
+    }
+
+    @Override
+    public void sendChatEvent(String username, String text) {
+        MapMessage mapMessage = new ActiveMQMapMessage();
+
+        try {
+            mapMessage.setString("userId", username);
+            mapMessage.setString("chatMessage", text);
+        } catch (JMSException e) {
+            throw new RuntimeException("JMSException thrown while trying to "
+                + "send chat event.");
+        }
+
+//        messageTemplate.send(JMS_CHAT, mapMessage);
+    }
+
+    @Override
+    public void sendGameStartingEvent(Game game) {
+//        messageTemplate.send(new GameStatusMessageCreator(game, GAME_IS_STARTING));
+    }
+
+    @Override
+    public void sendGameInProgressEvent(Game game) {
+//        messageTemplate.send(new GameStatusMessageCreator(game, GAME_IN_PROGRESS));
+    }
+
+    @Override
+    public void sendGameIsFinishedEvent(Game game) {
+//        messageTemplate.send(new GameStatusMessageCreator(game, GAME_IS_FINISHED));
+    }
+
+    @Override
+    public void sendNewHandStartingEvent(Game game, Table table) {
+//        messageTemplate.send(new TableStatusMessageCreator(game, table, NEW_HAND_STARTING));
+    }
+
+    @Override
+    public void sendNewHandStartingEventForAllTables(Game game, List<Table> tables) {
+        for (Table table : tables) {
+            sendNewHandStartingEvent(game, table);
+        }
+    }
+
+    private void sendDealFlopEvent(Game game, Table table) {
+//        messageTemplate.send(new TableStatusMessageCreator(game, table, DEAL_FLOP));
+    }
+
+    private void sendDealRiverEvent(Game game, Table table) {
+//        messageTemplate.send(new TableStatusMessageCreator(game, table, DEAL_RIVER));
+    }
+
+    private void sendDealTurnEvent(Game game, Table table) {
+//        messageTemplate.send(new TableStatusMessageCreator(game, table, DEAL_TURN));
+    }
+
+    private void sendHandCompleteEvent(Game game, Table table) {
+//        messageTemplate.send(new TableStatusMessageCreator(game, table, HAND_COMPLETE));
+    }
+
+    private void sendUserActedEvent(Game game, Table table) {
+//        messageTemplate.send(new TableStatusMessageCreator(game, table, USER_ACTED));
+    }
+
+    @Override
+    public void sendCheckEvent(Game game, Table table, HandState handState, String username) {
+        sendChatEvent("System", username + " checks.");
+        determineNextEvent(game, table, handState);
+
+    }
+
+    @Override
+    public void sendFoldEvent(Game game, Table table, HandState handState, String username) {
+        sendChatEvent("System", username + " folds.");
+        determineNextEvent(game, table, handState);
+    }
+
+    @Override
+    public void sendCallEvent(Game game, Table table, HandState handState, String username) {
+        sendChatEvent("System", username + " calls.");
+        determineNextEvent(game, table, handState);
+    }
+
+    @Override
+    public void sendRaiseEvent(Game game, Table table, HandState handState, String username) {
+        sendChatEvent("System", username + " raises.");
+        determineNextEvent(game, table, handState);
+    }
+
+    private void determineNextEvent(Game game, Table table, HandState handState) {
+        if (handState.getHandRoundState() == HandRoundState.ROUND_COMPLETE) {
+            switch (handState.getHandDealerState()) {
+                case FLOP_DEALT:
+                    sendDealFlopEvent(game, table);
+                    break;
+                case TURN_DEALT:
+                    sendDealTurnEvent(game, table);
+                    break;
+                case RIVER_DEALT:
+                    sendDealRiverEvent(game, table);
+                    break;
+                case COMPLETE:
+                    sendHandCompleteEvent(game, table);
+                    break;
+                default:
+                    throw new IllegalStateException("The game is not in the "
+                            + "correct state.  One of the above round complete "
+                            + "events should have been sent.");
+            }
+        } else {
+            switch (handState.getHandDealerState()) {
+                case POCKET_CARDS_DEALT:
+                case FLOP_DEALT:
+                case TURN_DEALT:
+                case RIVER_DEALT:
+                    sendUserActedEvent(game, table);
+                    break;
+                default:
+                    throw new IllegalStateException("The game is not in the "
+                            + "correct state.  The HandDealerState should be "
+                            + "in one of the above states.");
+            }
+        }
+    }
+
+//    private abstract class FlexPokerMessageCreator implements AsyncMessageCreator {
+//
+//        protected String fullSubtopic;
+//
+//        protected String destination;
+//
+//        @Override
+//        public AsyncMessage createMessage() {
+//            AsyncMessage message = messageTemplate
+//                    .createMessageForDestination(destination);
+//            message.setHeader(AsyncMessage.SUBTOPIC_HEADER_NAME, fullSubtopic);
+//            return message;
+//        }
+//
+//    }
+//
+//    private class TableStatusMessageCreator extends FlexPokerMessageCreator {
+//
+//        public TableStatusMessageCreator(Game game, Table table, String subtopic) {
+//            super();
+//            if (game == null || game.getId() == null) {
+//                throw new IllegalArgumentException("Game or game id cannot "
+//                        + "be null.");
+//            }
+//            if (table == null || table.getId() == null) {
+//                throw new IllegalArgumentException("Table or table id cannot "
+//                        + "be null.");
+//            }
+//            if (StringUtils.isBlank(subtopic)) {
+//                throw new IllegalArgumentException("Subtopic cannot be blank.");
+//            }
+//            fullSubtopic = game.getId() + "." + table.getId() + "." + subtopic;
+//            destination = TABLE_STATUS_UPDATES;
+//        }
+//
+//    }
+//
+//    private class GameStatusMessageCreator extends FlexPokerMessageCreator {
+//
+//        public GameStatusMessageCreator(Game game, String subtopic) {
+//            super();
+//            if (game == null || game.getId() == null) {
+//                throw new IllegalArgumentException("Game or game id cannot "
+//                        + "be null.");
+//            }
+//            if (StringUtils.isBlank(subtopic)) {
+//                throw new IllegalArgumentException("Subtopic cannot be blank.");
+//            }
+//            fullSubtopic = game.getId() + "." + subtopic;
+//            destination = GAME_STATUS_UPDATES;
+//        }
+//
+//    }
 
 }
--- src/main/java/com/flexpoker/controller/FlexController.java
+++ src/main/java/com/flexpoker/controller/FlexController.java
@@ -3,59 +3,221 @@ package com.flexpoker.controller;
 import java.util.List;
 import java.util.Set;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+
+import com.flexpoker.bso.DealCardActionsBso;
+import com.flexpoker.bso.GameBso;
+import com.flexpoker.bso.GameEventBso;
+import com.flexpoker.bso.PlayerActionsBso;
 import com.flexpoker.model.FlopCards;
 import com.flexpoker.model.Game;
+import com.flexpoker.model.GameStage;
+import com.flexpoker.model.HandState;
 import com.flexpoker.model.PocketCards;
 import com.flexpoker.model.RiverCard;
 import com.flexpoker.model.Table;
 import com.flexpoker.model.TurnCard;
+import com.flexpoker.model.User;
 import com.flexpoker.model.UserGameStatus;
 
 /**
- * This is the interface for all interactivity between the Flex/Flash client
- * and the Java back-end.
+ * This implements the main and only interface used to communicate between the
+ * Flex/Flash client application and the Java back-end.
  *
- * Even though this interface will grow to be very large, for simplicity, no
- * other interfaces or remote destinations should be exposed to the client.  The
- * single exception to this is the Spring Security integration, which is
- * implicitly used for channel login.
+ * This class will grow very large due to the large number of methods, which is
+ * why each method must be as short as possible.
+ *
+ * Certain guidelines to be observed:
+ *
+ * 1. This class handles fetching the current user from the SecurityContext.
+ *
+ * 2. This class handles coordinating when events should be sent to the
+ *    subscribed clients, but should not actually handle sending the events.
+ *
+ * 3. The BSOs know more about when certain chunks of code need to be
+ *    thread-safe.  No 'synchronized' blocks should exist in this class.
+ *
+ * 4. As little logic as possible should be performed in each method.  Depending
+ *    on the method, each method should attempt to only contain three
+ *    lines/chunks of calls.  Most, if not all, methods should be no longer than
+ *    three lines.
+ *
+ *        a) Fetch the user.
+ *        b) Call some business logic.
+ *        c) Send the appropriate event.
  *
  * @author cwoolner
  */
-public interface FlexController {
+@Controller("flexController")
+public class FlexControllerImpl implements FlexController {
 
-    List<Game> fetchAllGames();
+    private GameBso gameBso;
 
-    void createGame(Game game);
+    private GameEventBso gameEventBso;
 
-    void joinGame(Game game);
+    private EventManager eventManager;
 
-    Set<UserGameStatus> fetchAllUserGameStatuses(Game game);
+    private DealCardActionsBso dealCardActionsBso;
 
-    void verifyRegistrationForGame(Game game);
+    private PlayerActionsBso playerActionsBso;
 
-    PocketCards fetchPocketCards(Game game, Table table);
+    @Override
+    public void createGame(Game game) {
+        User user = extractCurrentUser();
+        gameBso.createGame(user, game);
+        eventManager.sendGamesUpdatedEvent();
+    }
 
-    Table fetchTable(Game game, Table table);
+    @Override
+    public List<Game> fetchAllGames() {
+        return gameBso.fetchAllGames();
+    }
 
-    Table fetchPlayersCurrentTable(Game game);
+    @Override
+    public void joinGame(Game game) {
+        User user = extractCurrentUser();
+        boolean gameAtUserMax = gameEventBso.addUserToGame(user, game);
+        eventManager.sendUserJoinedEvent(game, user.getUsername(), gameAtUserMax);
+    }
+    
+    @Override
+    public Set<UserGameStatus> fetchAllUserGameStatuses(Game game) {
+        return gameBso.fetchUserGameStatuses(game);
+    }
 
-    void verifyGameInProgress(Game game);
+    @Override
+    public void verifyRegistrationForGame(Game game) {
+        User user = extractCurrentUser();
+        if (gameEventBso.verifyRegistration(user, game)) {
+            eventManager.sendGameInProgressEvent(game);
+        }
+    }
 
-    void verifyReadyToStartNewHand(Game game, Table table);
+    @Override
+    public void verifyGameInProgress(Game game) {
+        User user = extractCurrentUser();
+        if (gameEventBso.verifyGameInProgress(user, game)) {
+            eventManager.sendNewHandStartingEventForAllTables(game,
+                    gameBso.fetchTables(game));
+        }
+    }
 
-    void check(Game game, Table table);
+    @Override
+    public void verifyReadyToStartNewHand(Game game, Table table) {
+        User user = extractCurrentUser();
+        if (gameEventBso.verifyReadyToStartNewHand(user, game, table)) {
+            game = gameBso.fetchGame(game);
+            if (GameStage.FINISHED.equals(game.getGameStage())) {
+                eventManager.sendGameIsFinishedEvent(game);
+            } else {
+                eventManager.sendNewHandStartingEvent(game, table);
+            }
+        }
+    }
 
-    void fold(Game game, Table table);
+    @Override
+    public Table fetchPlayersCurrentTable(Game game) {
+        User user = extractCurrentUser();
+        return gameBso.fetchPlayersCurrentTable(user, game);
+    }
 
-    void call(Game game, Table table);
+    @Override
+    public Table fetchTable(Game game, Table table) {
+        return gameBso.fetchTable(game, table);
+    }
 
-    void raise(Game game, Table table, String raiseAmount);
+    @Override
+    public PocketCards fetchPocketCards(Game game, Table table) {
+        User user = extractCurrentUser();
+        return dealCardActionsBso.fetchPocketCards(user, game, table);
+    }
 
-    FlopCards fetchFlopCards(Game game, Table table);
+    @Override
+    public void check(Game game, Table table) {
+        User user = extractCurrentUser();
+        HandState handState = playerActionsBso.check(game, table, user);
+        eventManager.sendCheckEvent(game, table, handState, user.getUsername());
+    }
 
-    TurnCard fetchTurnCard(Game game, Table table);
+    @Override
+    public void fold(Game game, Table table) {
+        User user = extractCurrentUser();
+        HandState handState = playerActionsBso.fold(game, table, user);
+        eventManager.sendFoldEvent(game, table, handState, user.getUsername());
+    }
 
-    RiverCard fetchRiverCard(Game game, Table table);
+    @Override
+    public void call(Game game, Table table) {
+        User user = extractCurrentUser();
+        HandState handState = playerActionsBso.call(game, table, user);
+        eventManager.sendCallEvent(game, table, handState, user.getUsername());
+    }
+
+    @Override
+    public void raise(Game game, Table table, String raiseAmount) {
+        User user = extractCurrentUser();
+        HandState handState = playerActionsBso.raise(game, table, user, raiseAmount);
+        eventManager.sendRaiseEvent(game, table, handState, user.getUsername());
+    }
+
+    @Override
+    public FlopCards fetchFlopCards(Game game, Table table) {
+        return dealCardActionsBso.fetchFlopCards(game, table);
+    }
+
+    @Override
+    public RiverCard fetchRiverCard(Game game, Table table) {
+        return dealCardActionsBso.fetchRiverCard(game, table);
+    }
+
+    @Override
+    public TurnCard fetchTurnCard(Game game, Table table) {
+        return dealCardActionsBso.fetchTurnCard(game, table);
+    }
+
+    private User extractCurrentUser() {
+        return (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    }
+
+    public GameBso getGameBso() {
+        return gameBso;
+    }
+
+    public void setGameBso(GameBso gameBso) {
+        this.gameBso = gameBso;
+    }
+
+    public GameEventBso getGameEventBso() {
+        return gameEventBso;
+    }
+
+    public void setGameEventBso(GameEventBso gameEventBso) {
+        this.gameEventBso = gameEventBso;
+    }
+
+    public EventManager getEventManager() {
+        return eventManager;
+    }
+
+    public void setEventManager(EventManager eventManager) {
+        this.eventManager = eventManager;
+    }
+
+    public DealCardActionsBso getDealCardActionsBso() {
+        return dealCardActionsBso;
+    }
+
+    public void setDealCardActionsBso(DealCardActionsBso dealCardActionsBso) {
+        this.dealCardActionsBso = dealCardActionsBso;
+    }
+
+    public PlayerActionsBso getPlayerActionsBso() {
+        return playerActionsBso;
+    }
+
+    public void setPlayerActionsBso(PlayerActionsBso playerActionsBso) {
+        this.playerActionsBso = playerActionsBso;
+    }
 
 }
