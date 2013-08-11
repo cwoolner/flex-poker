aa22ee933e96e603b004fb7044ce10ec9fc823f1
Chris Woolner <chris.woolner@gmail.com>
2013-08-11 04:40:56 -0400

    Remove unused doSomething() method from the login controller.

--- src/main/java/com/flexpoker/controller/LoginController.java
+++ src/main/java/com/flexpoker/controller/LoginController.java
@@ -1,31 +1,15 @@
 package com.flexpoker.controller;
 
-import javax.inject.Inject;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.flexpoker.bso.api.GameBso;
-
 @Controller
 public class LoginController {
 
-    private final GameBso gameBso;
-    
-    @Inject
-    public LoginController(GameBso gameBso) {
-        this.gameBso = gameBso;
-    }
-    
     @RequestMapping(value="/login", method = RequestMethod.GET)
     public String index() {
         return "login";
     }
-    
-    @RequestMapping(value="/dosomething", method = RequestMethod.GET)
-    public String doSomething() {
-        gameBso.doSomething();
-        return null;
-    }
+
 }
