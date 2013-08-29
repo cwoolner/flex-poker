15bc017b3c69509b70dcc5a29c1f07cf441c481b
Chris Woolner <chris.woolner@gmail.com>
2013-08-29 09:05:20 -0400

    Add a chat window to the game view and subscribe/publish to the necessary topics.

--- src/main/webapp/resources/scripts/controllers/gameController.js
+++ src/main/webapp/resources/scripts/controllers/gameController.js
@@ -1,3 +1,45 @@
-flexpokerModule.controller('GameController', ['$scope', '$routeParams', function($scope, $routeParams) {
+flexpokerModule.controller('GameController', ['$scope', '$rootScope', '$routeParams', 'ngstomp', function($scope, $rootScope, $routeParams, ngstomp) {
     $scope.gameId = $routeParams['gameId'];
+
+    $scope.chatDisplay = '';
+    
+    if ($scope.client === undefined) {
+        $scope.client = ngstomp(new SockJS(rootUrl + 'application'));
+    }
+
+    $scope.client.connect("", "", function() {
+        
+        $scope.client.subscribe('/topic/chat/game/user/' + $scope.gameId, function(message) {
+            var scrollHeight = $('.chat-display').prop('scrollHeight');
+            $('.chat-display').prop('scrollTop', scrollHeight);
+            $scope.chatDisplay += $.parseJSON(message.body) + '\n';
+        });
+        $scope.client.subscribe('/topic/chat/game/system/' + $scope.gameId, function(message) {
+            var scrollHeight = $('.chat-display').prop('scrollHeight');
+            $('.chat-display').prop('scrollTop', scrollHeight);
+            $scope.chatDisplay += $.parseJSON(message.body) + '\n';
+        });
+    }, function() {}, '/');
+    
+    if ($rootScope.stompClients === undefined) {
+        $rootScope.stompClients = [];
+    }
+    $rootScope.stompClients.push($scope.client);
+
+    $scope.sendChat = function() {
+        if ($scope.chatMessage == '') {
+            return;
+        }
+
+        var gameMessage = {
+                message: $scope.chatMessage,
+                receiverUsernames: null,
+                gameId: $scope.gameId,
+                tableId: null
+        };
+
+        $scope.client.send('/app/sendchatmessage', {}, JSON.stringify(gameMessage)); 
+        $scope.chatMessage = '';
+    };
+
 }]);
