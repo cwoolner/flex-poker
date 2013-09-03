bc3079087bea2b8512b09a81d44ca430f43517d3
Chris Woolner <chris.woolner@gmail.com>
2013-09-02 23:27:14 -0400

    Change the game stage to inprogress.  After the timer is done.

--- src/main/java/com/flexpoker/core/scheduling/ScheduleMoveGameToInProgressJdkTimerCommand.java
+++ src/main/java/com/flexpoker/core/scheduling/ScheduleMoveGameToInProgressJdkTimerCommand.java
@@ -7,7 +7,9 @@ import javax.inject.Inject;
 
 import com.flexpoker.config.Command;
 import com.flexpoker.core.api.chat.SendGameChatMessageCommand;
+import com.flexpoker.core.api.game.ChangeGameStageCommand;
 import com.flexpoker.core.api.scheduling.ScheduleMoveGameToInProgressCommand;
+import com.flexpoker.model.GameStage;
 import com.flexpoker.model.chat.outgoing.GameChatMessage;
 
 @Command
@@ -15,9 +17,14 @@ public class ScheduleMoveGameToInProgressJdkTimerCommand implements ScheduleMove
 
     private final SendGameChatMessageCommand sendGameChatMessageCommand;
     
+    private final ChangeGameStageCommand changeGameStageCommand;
+    
     @Inject
-    public ScheduleMoveGameToInProgressJdkTimerCommand(SendGameChatMessageCommand sendGameChatMessageCommand) {
+    public ScheduleMoveGameToInProgressJdkTimerCommand(
+            SendGameChatMessageCommand sendGameChatMessageCommand,
+            ChangeGameStageCommand changeGameStageCommand) {
         this.sendGameChatMessageCommand = sendGameChatMessageCommand;
+        this.changeGameStageCommand = changeGameStageCommand;
     }
     
     @Override
@@ -27,6 +34,7 @@ public class ScheduleMoveGameToInProgressJdkTimerCommand implements ScheduleMove
             
             @Override
             public void run() {
+                changeGameStageCommand.execute(gameId, GameStage.INPROGRESS);
                 sendGameChatMessageCommand.execute(new GameChatMessage(
                         "Game is starting", null, true, gameId));
                 timer.cancel();
