086c9858962e720f3c5ebeea66311150b864c49a
Chris Woolner <chris.woolner@gmail.com>
2013-09-23 22:19:41 -0400

    Move more of the game initialization logic from the old bso to the new command.  This adds the dealt cards and adds blinds to the pot.

--- src/main/java/com/flexpoker/bso/GameEventBsoImpl.java
+++ src/main/java/com/flexpoker/bso/GameEventBsoImpl.java
@@ -43,7 +43,6 @@ public class GameEventBsoImpl {
 
     private final GameBso gameBso;
 
-    private final DeckBso deckBso;
 
     private final RealTimeGameRepository realTimeGameBso;
 
@@ -53,8 +52,6 @@ public class GameEventBsoImpl {
 
     private final HandEvaluatorBso handEvaluatorBso;
 
-    private final PotBso potBso;
-    
     @Inject
     public GameEventBsoImpl(GameBso gameBso, DeckBso deckBso,
             RealTimeGameRepository realTimeGameRepository,
@@ -62,124 +59,10 @@ public class GameEventBsoImpl {
             SetSeatStatusForNewGameCommand setSeatStatusForNewGameCommand,
             HandEvaluatorBso handEvaluatorBso, PotBso potBso) {
         this.gameBso = gameBso;
-        this.deckBso = deckBso;
         this.realTimeGameBso = realTimeGameRepository;
         this.setSeatStatusForNewHandCommand = setSeatStatusForNewHandCommand;
         this.setSeatStatusForNewGameCommand = setSeatStatusForNewGameCommand;
         this.handEvaluatorBso = handEvaluatorBso;
-        this.potBso = potBso;
-    }
-
-    private void startNewHand(Game game, Table table) {
-        setSeatStatusForNewHandCommand.execute(table);
-        resetTableStatus(game, table);
-    }
-
-    private void createNewRealTimeHand(Game game, Table table) {
-        RealTimeGame realTimeGame = realTimeGameBso.get(game.getId());
-        Blinds currentBlinds = realTimeGame.getCurrentBlinds();
-        int smallBlind = currentBlinds.getSmallBlind();
-        int bigBlind = currentBlinds.getBigBlind();
-
-        RealTimeHand realTimeHand = new RealTimeHand(table.getSeats());
-        Seat smallBlindSeat = (Seat) CollectionUtils.find(table.getSeats(),
-                new SmallBlindSeatPredicate());
-        Seat bigBlindSeat = (Seat) CollectionUtils.find(table.getSeats(),
-                new BigBlindSeatPredicate());
-
-
-        for (Seat seat : table.getSeats()) {
-            if (seat.getUserGameStatus() == null) {
-                continue;
-            }
-
-            int chipsInFront = 0;
-            int callAmount = bigBlind;
-            int raiseToAmount = bigBlind * 2;
-
-            if (seat.equals(bigBlindSeat)) {
-                chipsInFront = bigBlind;
-                callAmount = 0;
-            } else if (seat.equals(smallBlindSeat)) {
-                chipsInFront = smallBlind;
-                callAmount = smallBlind;
-            }
-
-            if (chipsInFront > seat.getUserGameStatus().getChips()) {
-                seat.setChipsInFront(seat.getUserGameStatus().getChips());
-            } else {
-                seat.setChipsInFront(chipsInFront);
-            }
-
-            seat.getUserGameStatus().setChips(
-                    seat.getUserGameStatus().getChips() - seat.getChipsInFront());
-
-            if (callAmount > seat.getUserGameStatus().getChips()) {
-                seat.setCallAmount(seat.getUserGameStatus().getChips());
-            } else {
-                seat.setCallAmount(callAmount);
-            }
-
-            int totalChips = seat.getUserGameStatus().getChips()
-                    + seat.getChipsInFront();
-
-            if (raiseToAmount > totalChips) {
-                seat.setRaiseTo(totalChips);
-            } else {
-                seat.setRaiseTo(raiseToAmount);
-            }
-
-            table.setTotalPotAmount(table.getTotalPotAmount()
-                    + seat.getChipsInFront());
-
-            if (seat.getRaiseTo() > 0) {
-                realTimeHand.addPossibleSeatAction(seat, GameEventType.RAISE);
-            }
-
-            if (seat.getCallAmount() > 0) {
-                realTimeHand.addPossibleSeatAction(seat, GameEventType.CALL);
-                realTimeHand.addPossibleSeatAction(seat, GameEventType.FOLD);
-            } else {
-                realTimeHand.addPossibleSeatAction(seat, GameEventType.CHECK);
-            }
-        }
-
-        determineNextToAct(table, realTimeHand);
-        realTimeHand.setLastToAct(bigBlindSeat);
-
-        realTimeHand.setHandDealerState(HandDealerState.POCKET_CARDS_DEALT);
-        realTimeHand.setHandRoundState(HandRoundState.ROUND_IN_PROGRESS);
-
-        List<HandEvaluation> handEvaluations = determineHandEvaluations(game, table);
-        realTimeHand.setHandEvaluationList(handEvaluations);
-
-        realTimeGame.addRealTimeHand(table, realTimeHand);
-    }
-
-    private List<HandEvaluation> determineHandEvaluations(Game game, Table table) {
-        FlopCards flopCards = deckBso.fetchFlopCards(game, table);
-        TurnCard turnCard = deckBso.fetchTurnCard(game, table);
-        RiverCard riverCard = deckBso.fetchRiverCard(game, table);
-
-        CommonCards commonCards = new CommonCards(flopCards, turnCard, riverCard);
-
-        List<HandRanking> possibleHands = handEvaluatorBso.determinePossibleHands(commonCards);
-
-        List<HandEvaluation> handEvaluations = new ArrayList<HandEvaluation>();
-
-        for (Seat seat : table.getSeats()) {
-            if (seat.getUserGameStatus() != null
-                    && seat.getUserGameStatus().getUser() != null) {
-                User user = seat.getUserGameStatus().getUser();
-                PocketCards pocketCards = deckBso.fetchPocketCards(user, game, table);
-                HandEvaluation handEvaluation = handEvaluatorBso
-                        .determineHandEvaluation(commonCards, user, pocketCards,
-                         possibleHands);
-                handEvaluations.add(handEvaluation);
-            }
-        }
-
-        return handEvaluations;
     }
 
     private void determineLastToAct(Table table, RealTimeHand realTimeHand) {
@@ -215,48 +98,5 @@ public class GameEventBsoImpl {
         }
     }
 
-    private void determineNextToAct(Table table, RealTimeHand realTimeHand) {
-        List<Seat> seats = table.getSeats();
-        Seat actionOnSeat = (Seat) CollectionUtils.find(seats,
-                new ActionOnSeatPredicate());
-
-        int actionOnIndex = seats.indexOf(actionOnSeat);
-
-        for (int i = actionOnIndex + 1; i < seats.size(); i++) {
-            if (seats.get(i).isStillInHand()) {
-                realTimeHand.setNextToAct(seats.get(i));
-                return;
-            }
-        }
-
-        for (int i = 0; i < actionOnIndex; i++) {
-            if (seats.get(i).isStillInHand()) {
-                realTimeHand.setNextToAct(seats.get(i));
-                return;
-            }
-        }
-    }
-
-    private void startNewGameForAllTables(Game game) {
-        for (Table table : gameBso.fetchTables(game)) {
-            setSeatStatusForNewGameCommand.execute(table);
-            resetTableStatus(game, table);
-        }
-    }
-
-    private void resetTableStatus(Game game, Table table) {
-        table.setTotalPotAmount(0);
-        deckBso.shuffleDeck(game, table);
-        potBso.createNewHandPot(game, table);
-        createNewRealTimeHand(game, table);
-        determineTablePotAmounts(game, table);
-    }
-
-    private void determineTablePotAmounts(Game game, Table table) {
-        table.setPotAmounts(new ArrayList<Integer>());
-        for (Pot pot : potBso.fetchAllPots(game, table)) {
-            table.getPotAmounts().add(pot.getAmount());
-        }
-    }
 
 }
--- src/main/java/com/flexpoker/core/game/InitializeAndStartGameImplCommand.java
+++ src/main/java/com/flexpoker/core/game/InitializeAndStartGameImplCommand.java
@@ -1,5 +1,7 @@
 package com.flexpoker.core.game;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -7,18 +9,41 @@ import java.util.UUID;
 
 import javax.inject.Inject;
 
+import org.apache.commons.collections.CollectionUtils;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
 
+import com.flexpoker.bso.api.DeckBso;
+import com.flexpoker.bso.api.HandEvaluatorBso;
+import com.flexpoker.bso.api.PotBso;
 import com.flexpoker.config.Command;
 import com.flexpoker.core.api.game.InitializeAndStartGameCommand;
 import com.flexpoker.core.api.seatstatus.SetSeatStatusForNewGameCommand;
 import com.flexpoker.core.api.tablebalancer.AssignInitialTablesForNewGame;
+import com.flexpoker.model.Blinds;
+import com.flexpoker.model.CommonCards;
+import com.flexpoker.model.FlopCards;
+import com.flexpoker.model.Game;
+import com.flexpoker.model.GameEventType;
+import com.flexpoker.model.HandDealerState;
+import com.flexpoker.model.HandEvaluation;
+import com.flexpoker.model.HandRanking;
+import com.flexpoker.model.HandRoundState;
+import com.flexpoker.model.PocketCards;
+import com.flexpoker.model.Pot;
 import com.flexpoker.model.RealTimeGame;
+import com.flexpoker.model.RealTimeHand;
+import com.flexpoker.model.RiverCard;
 import com.flexpoker.model.Seat;
 import com.flexpoker.model.Table;
+import com.flexpoker.model.TurnCard;
+import com.flexpoker.model.User;
 import com.flexpoker.model.UserGameStatus;
+import com.flexpoker.repository.api.GameRepository;
 import com.flexpoker.repository.api.RealTimeGameRepository;
+import com.flexpoker.util.ActionOnSeatPredicate;
+import com.flexpoker.util.BigBlindSeatPredicate;
 import com.flexpoker.util.MessagingConstants;
+import com.flexpoker.util.SmallBlindSeatPredicate;
 
 @Command
 public class InitializeAndStartGameImplCommand implements InitializeAndStartGameCommand {
@@ -31,16 +56,32 @@ public class InitializeAndStartGameImplCommand implements InitializeAndStartGame
     
     private final SetSeatStatusForNewGameCommand setSeatStatusForNewGameCommand;
     
+    private final GameRepository gameRepository;
+    
+    private final DeckBso deckBso;
+
+    private final PotBso potBso;
+    
+    private final HandEvaluatorBso handEvaluatorBso;
+    
     @Inject
     public InitializeAndStartGameImplCommand(
             AssignInitialTablesForNewGame assignInitialTablesForNewGame,
             RealTimeGameRepository realTimeGameRepository,
             SimpMessageSendingOperations messagingTemplate,
-            SetSeatStatusForNewGameCommand setSeatStatusForNewGameCommand) {
+            SetSeatStatusForNewGameCommand setSeatStatusForNewGameCommand,
+            GameRepository gameRepository,
+            DeckBso deckBso,
+            PotBso potBso,
+            HandEvaluatorBso handEvaluatorBso) {
         this.assignInitialTablesForNewGame = assignInitialTablesForNewGame;
         this.realTimeGameRepository = realTimeGameRepository;
         this.messagingTemplate = messagingTemplate;
         this.setSeatStatusForNewGameCommand = setSeatStatusForNewGameCommand;
+        this.gameRepository = gameRepository;
+        this.deckBso = deckBso;
+        this.potBso = potBso;
+        this.handEvaluatorBso = handEvaluatorBso;
     }
     
     @Override
@@ -71,6 +112,8 @@ public class InitializeAndStartGameImplCommand implements InitializeAndStartGame
             public void run() {
                 for (Table table: realTimeGame.getTables()) {
                     setSeatStatusForNewGameCommand.execute(table);
+                    Game game = gameRepository.findById(gameId);
+                    resetTableStatus(game, table);
                     messagingTemplate.convertAndSend(String.format(
                             MessagingConstants.TABLE_STATUS, gameId, table.getId()),
                             table);
@@ -80,6 +123,149 @@ public class InitializeAndStartGameImplCommand implements InitializeAndStartGame
         }, 5000);
     }
     
+    private void resetTableStatus(Game game, Table table) {
+        table.setTotalPotAmount(0);
+        deckBso.shuffleDeck(game, table);
+        potBso.createNewHandPot(game, table);
+        createNewRealTimeHand(game, table);
+        determineTablePotAmounts(game, table);
+    }
+    
+    private void createNewRealTimeHand(Game game, Table table) {
+        RealTimeGame realTimeGame = realTimeGameRepository.get(game.getId());
+        Blinds currentBlinds = realTimeGame.getCurrentBlinds();
+        int smallBlind = currentBlinds.getSmallBlind();
+        int bigBlind = currentBlinds.getBigBlind();
+
+        RealTimeHand realTimeHand = new RealTimeHand(table.getSeats());
+        Seat smallBlindSeat = (Seat) CollectionUtils.find(table.getSeats(),
+                new SmallBlindSeatPredicate());
+        Seat bigBlindSeat = (Seat) CollectionUtils.find(table.getSeats(),
+                new BigBlindSeatPredicate());
+
+        for (Seat seat : table.getSeats()) {
+            if (seat.getUserGameStatus() == null) {
+                continue;
+            }
+
+            int chipsInFront = 0;
+            int callAmount = bigBlind;
+            int raiseToAmount = bigBlind * 2;
+
+            if (seat.equals(bigBlindSeat)) {
+                chipsInFront = bigBlind;
+                callAmount = 0;
+            } else if (seat.equals(smallBlindSeat)) {
+                chipsInFront = smallBlind;
+                callAmount = smallBlind;
+            }
+
+            if (chipsInFront > seat.getUserGameStatus().getChips()) {
+                seat.setChipsInFront(seat.getUserGameStatus().getChips());
+            } else {
+                seat.setChipsInFront(chipsInFront);
+            }
+
+            seat.getUserGameStatus().setChips(
+                    seat.getUserGameStatus().getChips() - seat.getChipsInFront());
+
+            if (callAmount > seat.getUserGameStatus().getChips()) {
+                seat.setCallAmount(seat.getUserGameStatus().getChips());
+            } else {
+                seat.setCallAmount(callAmount);
+            }
+
+            int totalChips = seat.getUserGameStatus().getChips()
+                    + seat.getChipsInFront();
+
+            if (raiseToAmount > totalChips) {
+                seat.setRaiseTo(totalChips);
+            } else {
+                seat.setRaiseTo(raiseToAmount);
+            }
+
+            table.setTotalPotAmount(table.getTotalPotAmount()
+                    + seat.getChipsInFront());
+
+            if (seat.getRaiseTo() > 0) {
+                realTimeHand.addPossibleSeatAction(seat, GameEventType.RAISE);
+            }
+
+            if (seat.getCallAmount() > 0) {
+                realTimeHand.addPossibleSeatAction(seat, GameEventType.CALL);
+                realTimeHand.addPossibleSeatAction(seat, GameEventType.FOLD);
+            } else {
+                realTimeHand.addPossibleSeatAction(seat, GameEventType.CHECK);
+            }
+        }
+
+        determineNextToAct(table, realTimeHand);
+        realTimeHand.setLastToAct(bigBlindSeat);
+
+        realTimeHand.setHandDealerState(HandDealerState.POCKET_CARDS_DEALT);
+        realTimeHand.setHandRoundState(HandRoundState.ROUND_IN_PROGRESS);
+
+        List<HandEvaluation> handEvaluations = determineHandEvaluations(game, table);
+        realTimeHand.setHandEvaluationList(handEvaluations);
+
+        realTimeGame.addRealTimeHand(table, realTimeHand);
+    }
+
+    private List<HandEvaluation> determineHandEvaluations(Game game, Table table) {
+        FlopCards flopCards = deckBso.fetchFlopCards(game, table);
+        TurnCard turnCard = deckBso.fetchTurnCard(game, table);
+        RiverCard riverCard = deckBso.fetchRiverCard(game, table);
+
+        CommonCards commonCards = new CommonCards(flopCards, turnCard, riverCard);
+
+        List<HandRanking> possibleHands = handEvaluatorBso.determinePossibleHands(commonCards);
+
+        List<HandEvaluation> handEvaluations = new ArrayList<>();
+
+        for (Seat seat : table.getSeats()) {
+            if (seat.getUserGameStatus() != null
+                    && seat.getUserGameStatus().getUser() != null) {
+                User user = seat.getUserGameStatus().getUser();
+                PocketCards pocketCards = deckBso.fetchPocketCards(user, game, table);
+                HandEvaluation handEvaluation = handEvaluatorBso
+                        .determineHandEvaluation(commonCards, user, pocketCards,
+                         possibleHands);
+                handEvaluations.add(handEvaluation);
+            }
+        }
+
+        return handEvaluations;
+    }
+
+    private void determineNextToAct(Table table, RealTimeHand realTimeHand) {
+        List<Seat> seats = table.getSeats();
+        Seat actionOnSeat = (Seat) CollectionUtils.find(seats,
+                new ActionOnSeatPredicate());
+
+        int actionOnIndex = seats.indexOf(actionOnSeat);
+
+        for (int i = actionOnIndex + 1; i < seats.size(); i++) {
+            if (seats.get(i).isStillInHand()) {
+                realTimeHand.setNextToAct(seats.get(i));
+                return;
+            }
+        }
+
+        for (int i = 0; i < actionOnIndex; i++) {
+            if (seats.get(i).isStillInHand()) {
+                realTimeHand.setNextToAct(seats.get(i));
+                return;
+            }
+        }
+    }
+    
+    private void determineTablePotAmounts(Game game, Table table) {
+        table.setPotAmounts(new ArrayList<Integer>());
+        for (Pot pot : potBso.fetchAllPots(game, table)) {
+            table.getPotAmounts().add(pot.getAmount());
+        }
+    }
+
     private class OpenTableForUserDto {
 
         private final UUID gameId;
