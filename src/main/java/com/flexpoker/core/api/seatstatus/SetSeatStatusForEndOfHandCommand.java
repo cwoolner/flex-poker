24fb8c42df22abc39a90ea84529726dd123ae009
Chris Woolner <chris.woolner@gmail.com>
2015-02-07 20:08:12 -0500

    Remove a lot of core and repository classes that are no longer used and have had their logic extracted.

--- src/main/java/com/flexpoker/core/api/seatstatus/SetSeatStatusForEndOfHandCommand.java
+++
@@ -1,9 +0,0 @@
-package com.flexpoker.core.api.seatstatus;
-
-import com.flexpoker.model.Table;
-
-public interface SetSeatStatusForEndOfHandCommand {
-
-    void execute(Table table);
-
-}
--- src/test/java/com/flexpoker/core/seatstatus/SetSeatStatusForEndOfHandImplCommandTest.java
+++
@@ -1,5 +0,0 @@
-package com.flexpoker.core.seatstatus;
-
-public class SetSeatStatusForEndOfHandImplCommandTest {
-
-}
