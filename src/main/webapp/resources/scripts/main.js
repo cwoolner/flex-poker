d5b543be37a11298fd88c2aa889db183f4735ed6
Chris Woolner <chris.woolner@gmail.com>
2016-01-02 01:41:20 -0500

    Convert remaining functions to the fat arrow.  Remove the last couple usages of self/this.

--- src/main/webapp/resources/scripts/main.js
+++ src/main/webapp/resources/scripts/main.js
@@ -1,7 +1,7 @@
 import handleRoutes from './router/router.js';
 import webSocketService from './webSocketService.js';
 
-webSocketService.registerSubscription('/user/topic/chat/personal/user', function(message) { alert('personal' + message.body); });
-webSocketService.registerSubscription('/user/topic/chat/personal/system', function(message) { alert('personal' + message.body); });
+webSocketService.registerSubscription('/user/topic/chat/personal/user', message => alert('personal' + message.body));
+webSocketService.registerSubscription('/user/topic/chat/personal/system', message => alert('personal' + message.body));
 
 window.onhashchange = handleRoutes;
