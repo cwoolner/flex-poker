44e294c7461b7dd69504a4a9ee8a851ed449f77a
Chris Woolner <chris.woolner@gmail.com>
2016-05-22 05:52:14 -0400

    Small cleanup of logout function and cardData for consistency with other js files.

--- src/main/webapp/resources/modules/home/logout.js
+++ src/main/webapp/resources/modules/home/logout.js
@@ -1,26 +1,24 @@
 import webSocketService from '../common/webSocketService';
 
-function logout() {
-    webSocketService.disconnect();
+export default () => {
+  webSocketService.disconnect();
 
-    var token = document.querySelector("meta[name='_csrf']").content;
-    var header = document.querySelector("meta[name='_csrf_header']").content;
+  var token = document.querySelector("meta[name='_csrf']").content;
+  var header = document.querySelector("meta[name='_csrf_header']").content;
 
-    var myHeaders = new Headers();
-    myHeaders.append(header, token);
+  var myHeaders = new Headers();
+  myHeaders.append(header, token);
 
-    var myInit = {
-            method: 'POST',
-            headers: myHeaders,
-            cache: 'no-cache',
-            redirect: 'manual',
-            credentials: 'same-origin'
-    };
+  var myInit = {
+    method: 'POST',
+    headers: myHeaders,
+    cache: 'no-cache',
+    redirect: 'manual',
+    credentials: 'same-origin'
+  };
 
-    // the url coming back is actually blank becuase it's an opaqueredirect.
-    // this is fine for our purposes because we'll just be redirected to the
-    // login page anyway
-    fetch('/logout', myInit).then(response => location.href = response.url);
+  // the url coming back is actually blank becuase it's an opaqueredirect.
+  // this is fine for our purposes because we'll just be redirected to the
+  // login page anyway
+  fetch('/logout', myInit).then(response => location.href = response.url);
 }
-
-export default logout;
