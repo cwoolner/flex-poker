209f43dcbb58eacb0aa6abb23759a46f9809a59f
Chris Woolner <chris.woolner@gmail.com>
2016-08-21 14:32:44 -0400

    Change the way the card data array is created
    Use some constants and mapping functions to create the array.  This
    reduces the amount of lines of code and will make things easier to
    change in the future.

--- src/main/webapp/resources/modules/table/cardData.js
+++ src/main/webapp/resources/modules/table/cardData.js
@@ -1,54 +1,9 @@
-export default [
-  '/resources/img/nicubunu_Ornamental_deck_2_of_hearts.svg',
-  '/resources/img/nicubunu_Ornamental_deck_3_of_hearts.svg',
-  '/resources/img/nicubunu_Ornamental_deck_4_of_hearts.svg',
-  '/resources/img/nicubunu_Ornamental_deck_5_of_hearts.svg',
-  '/resources/img/nicubunu_Ornamental_deck_6_of_hearts.svg',
-  '/resources/img/nicubunu_Ornamental_deck_7_of_hearts.svg',
-  '/resources/img/nicubunu_Ornamental_deck_8_of_hearts.svg',
-  '/resources/img/nicubunu_Ornamental_deck_9_of_hearts.svg',
-  '/resources/img/nicubunu_Ornamental_deck_10_of_hearts.svg',
-  '/resources/img/nicubunu_Ornamental_deck_Jack_of_hearts.svg',
-  '/resources/img/nicubunu_Ornamental_deck_Queen_of_hearts.svg',
-  '/resources/img/nicubunu_Ornamental_deck_King_of_hearts.svg',
-  '/resources/img/nicubunu_Ornamental_deck_Ace_of_hearts.svg',
-  '/resources/img/nicubunu_Ornamental_deck_2_of_spades.svg',
-  '/resources/img/nicubunu_Ornamental_deck_3_of_spades.svg',
-  '/resources/img/nicubunu_Ornamental_deck_4_of_spades.svg',
-  '/resources/img/nicubunu_Ornamental_deck_5_of_spades.svg',
-  '/resources/img/nicubunu_Ornamental_deck_6_of_spades.svg',
-  '/resources/img/nicubunu_Ornamental_deck_7_of_spades.svg',
-  '/resources/img/nicubunu_Ornamental_deck_8_of_spades.svg',
-  '/resources/img/nicubunu_Ornamental_deck_9_of_spades.svg',
-  '/resources/img/nicubunu_Ornamental_deck_10_of_spades.svg',
-  '/resources/img/nicubunu_Ornamental_deck_Jack_of_spades.svg',
-  '/resources/img/nicubunu_Ornamental_deck_Queen_of_spades.svg',
-  '/resources/img/nicubunu_Ornamental_deck_King_of_spades.svg',
-  '/resources/img/nicubunu_Ornamental_deck_Ace_of_spades.svg',
-  '/resources/img/nicubunu_Ornamental_deck_2_of_diamonds.svg',
-  '/resources/img/nicubunu_Ornamental_deck_3_of_diamonds.svg',
-  '/resources/img/nicubunu_Ornamental_deck_4_of_diamonds.svg',
-  '/resources/img/nicubunu_Ornamental_deck_5_of_diamonds.svg',
-  '/resources/img/nicubunu_Ornamental_deck_6_of_diamonds.svg',
-  '/resources/img/nicubunu_Ornamental_deck_7_of_diamonds.svg',
-  '/resources/img/nicubunu_Ornamental_deck_8_of_diamonds.svg',
-  '/resources/img/nicubunu_Ornamental_deck_9_of_diamonds.svg',
-  '/resources/img/nicubunu_Ornamental_deck_10_of_diamonds.svg',
-  '/resources/img/nicubunu_Ornamental_deck_Jack_of_diamonds.svg',
-  '/resources/img/nicubunu_Ornamental_deck_Queen_of_diamonds.svg',
-  '/resources/img/nicubunu_Ornamental_deck_King_of_diamonds.svg',
-  '/resources/img/nicubunu_Ornamental_deck_Ace_of_diamonds.svg',
-  '/resources/img/nicubunu_Ornamental_deck_2_of_clubs.svg',
-  '/resources/img/nicubunu_Ornamental_deck_3_of_clubs.svg',
-  '/resources/img/nicubunu_Ornamental_deck_4_of_clubs.svg',
-  '/resources/img/nicubunu_Ornamental_deck_5_of_clubs.svg',
-  '/resources/img/nicubunu_Ornamental_deck_6_of_clubs.svg',
-  '/resources/img/nicubunu_Ornamental_deck_7_of_clubs.svg',
-  '/resources/img/nicubunu_Ornamental_deck_8_of_clubs.svg',
-  '/resources/img/nicubunu_Ornamental_deck_9_of_clubs.svg',
-  '/resources/img/nicubunu_Ornamental_deck_10_of_clubs.svg',
-  '/resources/img/nicubunu_Ornamental_deck_Jack_of_clubs.svg',
-  '/resources/img/nicubunu_Ornamental_deck_Queen_of_clubs.svg',
-  '/resources/img/nicubunu_Ornamental_deck_King_of_clubs.svg',
-  '/resources/img/nicubunu_Ornamental_deck_Ace_of_clubs.svg'
-];
+import _ from 'lodash';
+
+const urlTemplate = (cardSuit, cardRank) => `/resources/img/nicubunu_Ornamental_deck_${cardRank}_of_${cardSuit}.svg`;
+const cardSuits = ['hearts', 'spades', 'diamonds', 'clubs'];
+const cardRanks = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace'];
+
+export default _.flatten(
+  cardSuits.map(cardSuit => cardRanks.map(cardRank => urlTemplate(cardSuit, cardRank)))
+);
