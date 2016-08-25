2a9ba41a882bd3c175b11a8d5b6afa62db702862
Chris Woolner <chris.woolner@gmail.com>
2016-08-25 02:08:24 -0400

    Make create/join game dialogs stateless functional components

--- src/main/webapp/resources/modules/game/CreateGameDialog.js
+++ src/main/webapp/resources/modules/game/CreateGameDialog.js
@@ -2,19 +2,15 @@ import React from 'react';
 import WebSocketService from '../webSocket/WebSocketService';
 import { Modal, Button, FormControl, FormGroup, ControlLabel } from 'react-bootstrap';
 
-export default React.createClass({
+export default ({showModal, hideDialog}) => {
 
-  hideDialog() {
-    this.props.hideDialog();
-  },
-
-  preventNonNumeric(evt) {
+  const preventNonNumeric = (evt) => {
     if (!(evt.which >= 48 && evt.which <= 57)) {
       evt.preventDefault();
     }
-  },
+  };
 
-  createGameFormSubmitted(evt) {
+  const createGameFormSubmitted = (evt) => {
     evt.preventDefault();
     const nameElement = evt.target.elements[0];
     const playersElement = evt.target.elements[1];
@@ -28,41 +24,40 @@ export default React.createClass({
       numberOfMinutesBetweenBlindLevels: numberOfMinutesBetweenBlindLevelsElement.value
     });
 
-    this.hideDialog();
-  },
+    hideDialog();
+  };
 
-  render() {
-    return (
-      <Modal show={this.props.showModal} onHide={this.hideDialog} {...this.props}>
-        <Modal.Header>
-          <button className={"close"} onClick={this.hideDialog}>X</button>
-          <Modal.Title>Create Game</Modal.Title>
-        </Modal.Header>
-        <form id="create-game-form" onSubmit={this.createGameFormSubmitted}>
-          <Modal.Body>
-            <FormGroup>
-              <ControlLabel>Name</ControlLabel>
-              <FormControl type="text" name="name" autoFocus />
-            </FormGroup>
-            <FormGroup>
-              <ControlLabel>Number of Players</ControlLabel>
-              <FormControl type="text" name="players" onKeyPress={this.preventNonNumeric} />
-            </FormGroup>
-            <FormGroup>
-              <ControlLabel>Number of Players per Table</ControlLabel>
-              <FormControl type="text" name="playersPerTable" onKeyPress={this.preventNonNumeric} />
-            </FormGroup>
-            <FormGroup>
-              <ControlLabel>Blind increment (minutes)</ControlLabel>
-              <FormControl type="text" name="numberOfMinutesBetweenBlindLevels" onKeyPress={this.preventNonNumeric} />
-            </FormGroup>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={this.hideDialog}>Close</Button>
-            <Button type="submit" bsStyle="primary">Create Game</Button>
-          </Modal.Footer>
-        </form>
-      </Modal>
-    )
-  }
-})
+ return (
+    <Modal show={showModal} onHide={hideDialog}>
+      <Modal.Header>
+        <button className={"close"} onClick={hideDialog}>X</button>
+        <Modal.Title>Create Game</Modal.Title>
+      </Modal.Header>
+      <form id="create-game-form" onSubmit={createGameFormSubmitted}>
+        <Modal.Body>
+          <FormGroup>
+            <ControlLabel>Name</ControlLabel>
+            <FormControl type="text" name="name" autoFocus />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Number of Players</ControlLabel>
+            <FormControl type="text" name="players" onKeyPress={preventNonNumeric} />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Number of Players per Table</ControlLabel>
+            <FormControl type="text" name="playersPerTable" onKeyPress={preventNonNumeric} />
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Blind increment (minutes)</ControlLabel>
+            <FormControl type="text" name="numberOfMinutesBetweenBlindLevels" onKeyPress={preventNonNumeric} />
+          </FormGroup>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={hideDialog}>Close</Button>
+          <Button type="submit" bsStyle="primary">Create Game</Button>
+        </Modal.Footer>
+      </form>
+    </Modal>
+  );
+
+}
--- src/main/webapp/resources/modules/game/JoinGameDialog.js
+++ src/main/webapp/resources/modules/game/JoinGameDialog.js
@@ -3,47 +3,41 @@ import WebSocketService from '../webSocket/WebSocketService';
 import { Modal, Button, FormControl, FormGroup, ControlLabel } from 'react-bootstrap';
 import { hashHistory } from 'react-router';
 
-export default React.createClass({
+export default ({gameId, showModal, hideDialog}) => {
 
-  hideDialog() {
-    this.props.hideDialog();
-  },
-
-  joinGameFormSubmitted(evt) {
+  const joinGameFormSubmitted = (evt) => {
     evt.preventDefault();
-    WebSocketService.send('/app/joingame', this.props.gameId);
-    this.hideDialog();
-    hashHistory.push(`/game/${this.props.gameId}`);
-  },
+    WebSocketService.send('/app/joingame', gameId);
+    hideDialog();
+    hashHistory.push(`/game/${gameId}`);
+  };
 
-  render() {
-    return (
-      <Modal show={this.props.showModal} onHide={this.hideDialog} {...this.props}>
-        <Modal.Header>
-          <button className={"close"} onClick={this.hideDialog}>X</button>
-          <Modal.Title>Join Game</Modal.Title>
-        </Modal.Header>
-        <form id="join-game-form" onSubmit={this.joinGameFormSubmitted}>
-          <Modal.Body>
-            <FormGroup>
-              <ControlLabel>Current Balance</ControlLabel>
-              <FormControl.Static>100</FormControl.Static>
-            </FormGroup>
-            <FormGroup>
-              <ControlLabel>Cost</ControlLabel>
-              <FormControl.Static>10</FormControl.Static>
-            </FormGroup>
-            <FormGroup>
-              <ControlLabel>Remaining Balance</ControlLabel>
-              <FormControl.Static>90</FormControl.Static>
-            </FormGroup>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button onClick={this.hideDialog}>Close</Button>
-            <Button type="submit" bsStyle="primary" autoFocus>Join Game</Button>
-          </Modal.Footer>
-        </form>
-      </Modal>
-    )
-  }
-})
+  return (
+    <Modal show={showModal} onHide={hideDialog}>
+      <Modal.Header>
+        <button className={"close"} onClick={hideDialog}>X</button>
+        <Modal.Title>Join Game</Modal.Title>
+      </Modal.Header>
+      <form id="join-game-form" onSubmit={joinGameFormSubmitted}>
+        <Modal.Body>
+          <FormGroup>
+            <ControlLabel>Current Balance</ControlLabel>
+            <FormControl.Static>100</FormControl.Static>
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Cost</ControlLabel>
+            <FormControl.Static>10</FormControl.Static>
+          </FormGroup>
+          <FormGroup>
+            <ControlLabel>Remaining Balance</ControlLabel>
+            <FormControl.Static>90</FormControl.Static>
+          </FormGroup>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button onClick={hideDialog}>Close</Button>
+          <Button type="submit" bsStyle="primary" autoFocus>Join Game</Button>
+        </Modal.Footer>
+      </form>
+    </Modal>
+  );
+}
