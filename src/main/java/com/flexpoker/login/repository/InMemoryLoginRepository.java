adef83d6b5d1b39b5df4f19392b841876918a7db
Chris Woolner <chris.woolner@gmail.com>
2016-10-03 01:51:07 -0400

    Clean-up a couple of things in the in-memory login repo
    The double init issue between spring security and core spring contexts
    was fixed in a previous commit, so the static stuff is gone.  Also
    changed to use a Spring User class instead of implementing the
    UserDetail interface, which cut down on a bunch of code.

--- src/main/java/com/flexpoker/login/repository/InMemoryLoginRepository.java
+++ src/main/java/com/flexpoker/login/repository/InMemoryLoginRepository.java
@@ -1,14 +1,13 @@
 package com.flexpoker.login.repository;
 
-import java.util.Arrays;
-import java.util.Collection;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.context.annotation.Profile;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,80 +17,28 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class InMemoryLoginRepository implements LoginRepository {
 
-    private static final Map<String, UserDetails> loginUserMap;
+    private final Map<String, UserDetails> loginUserMap;
 
-    private static final Map<String, UUID> loginIdMap;
+    private final Map<String, UUID> loginIdMap;
 
-    private static final Map<UUID, String> aggregateIdUsernameMap;
+    private final Map<UUID, String> aggregateIdUsernameMap;
 
-    /**
-     * NOTE: Because this class is used by Spring Security, it is instantiated
-     * in two different contexts. This means that the previous Map instance
-     * variables were created twice. When the app stored a new player, it stored
-     * it into a different Map than the one read by Spring Security, so InMemory
-     * login was broken. Changing these to static fixes that issue. Not a huge
-     * deal since a separate persistence store will be used when deploying the
-     * system for real.
-     */
-    static {
+    public InMemoryLoginRepository() {
         loginUserMap = new HashMap<>();
         loginIdMap = new HashMap<>();
         aggregateIdUsernameMap = new HashMap<>();
-    }
-
-    public InMemoryLoginRepository() {
         addDefaultUsers();
     }
 
     @Override
-    public UserDetails loadUserByUsername(String username)
-            throws UsernameNotFoundException {
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return loginUserMap.get(username);
     }
 
     @Override
     public void saveUsernameAndPassword(String username, String encryptedPassword) {
-        UserDetails userDetails = new UserDetails() {
-
-            private static final long serialVersionUID = 1L;
-
-            @Override
-            public boolean isEnabled() {
-                return true;
-            }
-
-            @Override
-            public boolean isCredentialsNonExpired() {
-                return true;
-            }
-
-            @Override
-            public boolean isAccountNonLocked() {
-                return true;
-            }
-
-            @Override
-            public boolean isAccountNonExpired() {
-                return true;
-            }
-
-            @Override
-            public String getUsername() {
-                return username;
-            }
-
-            @Override
-            public String getPassword() {
-                return encryptedPassword;
-            }
-
-            @Override
-            public Collection<? extends GrantedAuthority> getAuthorities() {
-                return Arrays.asList(new SimpleGrantedAuthority("ROLE_USER"));
-            }
-        };
-
-        loginUserMap.put(username, userDetails);
+        loginUserMap.put(username, new User(username, encryptedPassword,
+                Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"))));
     }
 
     @Override
