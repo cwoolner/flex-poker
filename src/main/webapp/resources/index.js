3c38e9ff2cdb2ed2066fe806a5ad8b4f06ec8e5e
Chris Woolner <chris.woolner@gmail.com>
2018-05-10 00:56:19 -0400

    Move all of the chat subscription logic out into its own subscriber function that subscribes directly to the redux store and makes the websocket subs accordingly

--- src/main/webapp/resources/index.js
+++ src/main/webapp/resources/index.js
@@ -5,8 +5,11 @@ import { Provider } from 'react-redux';
 import WebSocketSubscriptionManager from './modules/webSocket/WebSocketSubscriptionManager';
 import reducer from './reducers';
 import App from './App';
+import chatMessageSubscriber from './modules/home/Chat/chatMessageSubscriber'
 
 const store = createStore(reducer);
+
+store.subscribe(chatMessageSubscriber(store))
 
 render((
   <Provider store={store}>
--- src/main/webapp/resources/modules/home/Chat/index.js
+++ src/main/webapp/resources/modules/home/Chat/index.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import { List, Map } from 'immutable'
 import { FormGroup, FormControl, FieldGroup, Button } from 'react-bootstrap';
-import WebSocketSubscriptionManager from '../../webSocket/WebSocketSubscriptionManager';
 import WebSocketService from '../../webSocket/WebSocketService';
 import { connect } from 'react-redux'
-import { globalChatMsgReceived, gameChatMsgReceived, tableChatMsgReceived } from '../../../reducers';
 import ChatLine from './ChatLine'
 
 class Chat extends React.Component {
@@ -14,7 +12,6 @@ class Chat extends React.Component {
 
     this.displayAreaMutationObserverCallback = this.displayAreaMutationObserverCallback.bind(this)
     this.chatFormSubmitted = this.chatFormSubmitted.bind(this);
-    this.registerWebSocketSubs = this.registerWebSocketSubs.bind(this)
 
     this.displayArea = React.createRef()
     this.displayAreaMutationObserver = new MutationObserver(this.displayAreaMutationObserverCallback);
@@ -41,49 +38,15 @@ class Chat extends React.Component {
     }
   }
 
-  acceptGlobalChatMsg(dispatch, message) {
-    dispatch(globalChatMsgReceived(JSON.parse(message.body)))
-  }
-
-  acceptGameChatMsg(dispatch, gameId, message) {
-    dispatch(gameChatMsgReceived(gameId, JSON.parse(message.body)))
-  }
-
-  acceptTableChatMsg(dispatch, gameId, tableId, message) {
-    dispatch(tableChatMsgReceived(gameId, tableId, JSON.parse(message.body)))
-  }
-
-  registerWebSocketSubs(dispatch, activeChatStream) {
-    const { gameId, tableId } = activeChatStream || {}
-    if (gameId && tableId) {
-      WebSocketSubscriptionManager.subscribe(this, [{
-        location: `/topic/chat/game/${gameId}/table/${tableId}`,
-        subscription: this.acceptTableChatMsg.bind(this, dispatch, gameId, tableId)
-      }])
-    } else if (gameId) {
-      WebSocketSubscriptionManager.subscribe(this, [{
-        location: `/topic/chat/game/${gameId}`,
-        subscription: this.acceptGameChatMsg.bind(this, dispatch, gameId)
-      }])
-    } else {
-      WebSocketSubscriptionManager.subscribe(this, [{
-        location: '/topic/chat/global',
-        subscription: this.acceptGlobalChatMsg.bind(this, dispatch)
-      }])
-    }
-  }
-
   componentDidMount() {
     this.displayAreaMutationObserver.observe(this.displayArea.current, { childList: true })
   }
 
   componentWillUnmount() {
-    WebSocketSubscriptionManager.unsubscribe(this);
     this.displayAreaMutationObserver.disconnect();
   }
 
   render() {
-    this.registerWebSocketSubs(this.props.dispatch, this.props.activeChatStream)
     return (
       <div className={'chat-area'}>
         <div className={'chat-text-display-area form-control form-group'} ref={this.displayArea}>
