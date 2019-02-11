af196ccf1a5c6ac0ff7db13ef749260da00e7402
Chris Woolner <chris.woolner@gmail.com>
2019-02-10 20:16:38 -0500

    Replace MenuItem with Dropdown.Item

--- src/main/webapp/resources/modules/home/GameTabs/GameTab.js
+++ src/main/webapp/resources/modules/home/GameTabs/GameTab.js
@@ -1,13 +1,14 @@
-import React from 'react';
-import { NavDropdown, MenuItem } from 'react-bootstrap';
+import React from 'react'
+import { NavDropdown } from 'react-bootstrap'
+import Dropdown from 'react-bootstrap/Dropdown'
 
 export default ({openGameTab}) => {
   return (
     <NavDropdown title={openGameTab.name}>
-      {openGameTab.myTableId ? <MenuItem href={`#/game/${openGameTab.gameId}/table/${openGameTab.myTableId}`}>My Table</MenuItem> : null}
-      <MenuItem href={`#/game/${openGameTab.gameId}`}>Game Page</MenuItem>
-      {openGameTab.viewingTables.length > 0 ? <MenuItem divider /> : null}
-      {openGameTab.viewingTables.map((viewingTable, index) => <MenuItem key={index} href={`#/game/${openGameTab.gameId}/table/${openGameTab.tableId}`} />)}
+      {openGameTab.myTableId ? <Dropdown.Item href={`#/game/${openGameTab.gameId}/table/${openGameTab.myTableId}`}>My Table</Dropdown.Item> : null}
+      <Dropdown.Item href={`#/game/${openGameTab.gameId}`}>Game Page</Dropdown.Item>
+      {openGameTab.viewingTables.length > 0 ? <Dropdown.Item divider /> : null}
+      {openGameTab.viewingTables.map((viewingTable, index) => <Dropdown.Item key={index} href={`#/game/${openGameTab.gameId}/table/${openGameTab.tableId}`} />)}
     </NavDropdown>
   )
 }
--- src/main/webapp/resources/modules/home/Navigation/index.js
+++ src/main/webapp/resources/modules/home/Navigation/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Nav, Navbar, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+import React from 'react'
+import { Nav, Navbar, NavItem, NavDropdown } from 'react-bootstrap'
+import Dropdown from 'react-bootstrap/Dropdown'
 
 export default ({username}) => {
   return (
@@ -11,10 +12,10 @@ export default ({username}) => {
       </Navbar.Header>
       <Nav pullRight>
         <NavDropdown eventKey={3} title={username} id="user-actions-dropdown">
-          <MenuItem eventKey={3.1}>Profile</MenuItem>
-          <MenuItem eventKey={3.2}>Account</MenuItem>
-          <MenuItem divider />
-          <MenuItem eventKey={3.3} href="#/logout">Logout</MenuItem>
+          <Dropdown.Item eventKey={3.1}>Profile</Dropdown.Item>
+          <Dropdown.Item eventKey={3.2}>Account</Dropdown.Item>
+          <Dropdown.Item divider />
+          <Dropdown.Item eventKey={3.3} href="#/logout">Logout</Dropdown.Item>
         </NavDropdown>
       </Nav>
     </Navbar>
