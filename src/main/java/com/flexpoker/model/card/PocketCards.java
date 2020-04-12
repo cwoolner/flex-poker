6dd40445939ec6ddf0875c2b5fa4ffef6670bee1
Chris Woolner <chris.woolner@gmail.com>
2020-04-12 02:02:42 -0400

    Switch the spring platform bom out for spring boot now that spring platform is no longer supported
    - Update the dep/code to commons-lang3 since commons-lang is no longer
      supported
    - Remove commons-logging since that is no longer supported

--- src/main/java/com/flexpoker/model/card/PocketCards.java
+++ src/main/java/com/flexpoker/model/card/PocketCards.java
@@ -1,7 +1,7 @@
 package com.flexpoker.model.card;
 
-import org.apache.commons.lang.builder.EqualsBuilder;
-import org.apache.commons.lang.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
