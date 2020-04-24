ee1ca0b52e3623af352d9aca6cc8625098161375
Chris Woolner <chris.woolner@gmail.com>
2020-04-24 01:31:34 -0400

    Revert "Remove the default @Profile annotations"
    This reverts commit a3fe008a0b6d1f291f3dbf763af65187dce5a45d.

--- src/main/java/com/flexpoker/signup/repository/InMemorySignUpRepository.java
+++ src/main/java/com/flexpoker/signup/repository/InMemorySignUpRepository.java
@@ -6,10 +6,12 @@ import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Repository;
 
 import com.flexpoker.signup.SignUpUser;
 
+@Profile("default")
 @Repository
 public class InMemorySignUpRepository implements SignUpRepository {
 
