ca2412c1632b3fc0750a3b664e1ed5b58beefbdc
Chris Woolner <chris.woolner@gmail.com>
2020-05-21 21:51:36 -0400

    Convert the game page over to use redux hooks

--- src/main/webapp/resources/modules/game/GamePage.js
+++ src/main/webapp/resources/modules/game/GamePage.js
@@ -1,18 +1,14 @@
 import React, { useEffect } from 'react'
-import { connect } from 'react-redux'
+import { useDispatch } from 'react-redux'
 import { changeChatMsgStream } from '../../reducers'
 
-const GamePage = ({ changeChatMsgStream, match: { params: { gameId }} }) => {
+export default ({ match: { params: { gameId }} }) => {
+
+  const dispatch = useDispatch()
 
   useEffect(() => {
-    changeChatMsgStream(gameId)
+    dispatch(changeChatMsgStream(gameId, null))
   }, [gameId])
 
   return <div>Game page</div>
 }
-
-const mapDispatchToProps = (dispatch) => ({
-  changeChatMsgStream: (gameId) => dispatch(changeChatMsgStream(gameId, null))
-})
-
-export default connect(null, mapDispatchToProps)(GamePage)
