3c362e539bb6f8ade2934348ff144f37b11d828a
Chris Woolner <chris.woolner@gmail.com>
2020-12-16 23:59:13 -0500

    Move/convert the Application class to kotlin

--- src/main/kotlin/com/flexpoker/Application.kt
+++ src/main/kotlin/com/flexpoker/Application.kt
@@ -1,13 +1,11 @@
 package com.flexpoker
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import kotlin.jvm.JvmStatic
-import org.springframework.boot.SpringApplication
+import org.springframework.boot.runApplication
 
 @SpringBootApplication
-object Application {
-    @JvmStatic
-    fun main(args: Array<String>) {
-        SpringApplication.run(Application::class.java, *args)
-    }
+class Application
+
+fun main(args: Array<String>) {
+    runApplication<Application>(*args)
 }
