f17425091212010d9999e846d698824fc8c526b3
Chris Woolner <chris.woolner@gmail.com>
2022-09-24 00:03:45 -0400

    Upgrade back-end libs.  Main change was upgrading kotlin version to 1.7, which required some when expression changes

--- src/main/kotlin/com/flexpoker/game/command/eventpublishers/InMemoryAsyncGameEventPublisher.kt
+++ src/main/kotlin/com/flexpoker/game/command/eventpublishers/InMemoryAsyncGameEventPublisher.kt
@@ -1,15 +1,16 @@
 package com.flexpoker.game.command.eventpublishers
 
+import com.flexpoker.framework.event.EventHandler
 import com.flexpoker.framework.event.EventPublisher
 import com.flexpoker.framework.event.subscriber.EventSubscriber
 import com.flexpoker.framework.processmanager.ProcessManager
+import com.flexpoker.game.command.events.GameEvent
 import com.flexpoker.game.command.events.GameStartedEvent
 import com.flexpoker.game.command.events.GameTablesCreatedAndPlayersAssociatedEvent
 import com.flexpoker.game.command.events.NewHandIsClearedToStartEvent
 import com.flexpoker.game.command.events.PlayerMovedToNewTableEvent
 import com.flexpoker.game.command.events.TablePausedForBalancingEvent
 import com.flexpoker.game.command.events.TableResumedAfterBalancingEvent
-import com.flexpoker.game.command.events.GameEvent
 import org.springframework.stereotype.Component
 import java.util.Timer
 import java.util.TimerTask
@@ -26,6 +27,10 @@ class InMemoryAsyncGameEventPublisher @Inject constructor(
     private val pauseTableProcessManager: ProcessManager<TablePausedForBalancingEvent>,
     private val resumeTableProcessManager: ProcessManager<TableResumedAfterBalancingEvent>
 ) : EventPublisher<GameEvent> {
+
+    companion object {
+        private val NOOP = EventHandler { x: GameEvent -> }
+    }
 
     override fun publish(event: GameEvent) {
         gameEventSubscriber.receive(event)
@@ -46,6 +51,7 @@ class InMemoryAsyncGameEventPublisher @Inject constructor(
             is PlayerMovedToNewTableEvent -> movePlayerBetweenTablesProcessManager.handle(event)
             is TablePausedForBalancingEvent -> pauseTableProcessManager.handle(event)
             is TableResumedAfterBalancingEvent -> resumeTableProcessManager.handle(event)
+            else -> NOOP
         }
     }
 
--- src/main/kotlin/com/flexpoker/table/command/eventpublishers/InMemoryAsyncTableEventPublisher.kt
+++ src/main/kotlin/com/flexpoker/table/command/eventpublishers/InMemoryAsyncTableEventPublisher.kt
@@ -1,5 +1,6 @@
 package com.flexpoker.table.command.eventpublishers
 
+import com.flexpoker.framework.event.EventHandler
 import com.flexpoker.framework.event.EventPublisher
 import com.flexpoker.framework.event.subscriber.EventSubscriber
 import com.flexpoker.framework.processmanager.ProcessManager
@@ -18,12 +19,17 @@ class InMemoryAsyncTableEventPublisher @Inject constructor(
     private val autoMoveHandForwardProcessManager: ProcessManager<AutoMoveHandForwardEvent>
 ) : EventPublisher<TableEvent> {
 
+    companion object {
+        private val NOOP = EventHandler { x: TableEvent -> }
+    }
+
     override fun publish(event: TableEvent) {
         tableEventSubscriber.receive(event)
         when (event) {
             is ActionOnChangedEvent -> actionOnCountdownProcessManager.handle(event)
             is HandCompletedEvent -> attemptToStartNewHandForExistingTableProcessManager.handle(event)
             is AutoMoveHandForwardEvent -> autoMoveHandForwardProcessManager.handle(event)
+            else -> NOOP
         }
     }
 
