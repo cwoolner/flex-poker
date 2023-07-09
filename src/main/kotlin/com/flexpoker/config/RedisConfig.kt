87eda198113c184dec5f867f34d3792049d5e88d
Chris Woolner <chris.woolner@gmail.com>
2023-07-08 20:29:32 -0400

    Switch to use lettuce from jedis just to simplify the pom deps

--- src/main/kotlin/com/flexpoker/config/RedisConfig.kt
+++ src/main/kotlin/com/flexpoker/config/RedisConfig.kt
@@ -16,7 +16,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Primary
 import org.springframework.context.annotation.Profile
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory
 import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer
 import org.springframework.data.redis.serializer.StringRedisSerializer
@@ -35,8 +35,8 @@ import java.util.UUID
 class RedisConfig {
 
     @Bean
-    fun jedisConnectionFactory(): JedisConnectionFactory {
-        return JedisConnectionFactory(RedisStandaloneConfiguration("localhost", 6379))
+    fun lettuceConnectionFactory(): LettuceConnectionFactory {
+        return LettuceConnectionFactory(RedisStandaloneConfiguration("localhost", 6379))
     }
 
     @Bean
@@ -132,7 +132,7 @@ class RedisConfig {
     }
 
     private fun setupConnectionAndKeySerializerRedisTemplate(redisTemplate: RedisTemplate<*, *>) {
-        redisTemplate.connectionFactory = jedisConnectionFactory()
+        redisTemplate.connectionFactory = lettuceConnectionFactory()
         redisTemplate.keySerializer = StringRedisSerializer()
     }
 
